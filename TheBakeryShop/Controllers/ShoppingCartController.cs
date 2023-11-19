1ba9bdb1209c8ba656a6edefe8a15605f4c6422e
LeChayS <shinoacute@gmail.com>
2023-11-19 07:28:27 +0700

    them cart controller

---
+++ TheBakeryShop/Controllers/ShoppingCartController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheBakeryShop.Models;
+
+namespace TheBakeryShop.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        DBBakeryShopEntities db = new DBBakeryShopEntities();
+        // GET: ShoppingCart
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
