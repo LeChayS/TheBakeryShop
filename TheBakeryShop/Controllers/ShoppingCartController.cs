bb1ae6d9c70fb6ce5da9bb11e74527e656de85a1
trangwvw <nttt4304@gmail.com>
2023-11-20 11:08:18 +0700

    sua nho

--- TheBakeryShop/Controllers/ShoppingCartController.cs
+++ TheBakeryShop/Controllers/ShoppingCartController.cs
@@ -11,7 +11,89 @@ namespace TheBakeryShop.Controllers
     {
         DBBakeryShopEntities db = new DBBakeryShopEntities();
         // GET: ShoppingCart
-        public ActionResult Index()
+        public ActionResult ShowCart()
+        {
+            if (Session["Cart"] == null)
+                //return RedirectToAction("ShowCart", "ShoppingCart");
+            return View("EmptyCart");
+            Cart _cart = Session["Cart"] as Cart;
+            return View(_cart);
+        }
+        public Cart GetCart()
+        {
+            Cart cart = Session["Cart"] as Cart;
+            if (cart == null || Session["Cart"] == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+        public ActionResult AddToCart(string id)
+        {
+            var _pro = db.tbProducts.SingleOrDefault(s => s.codePro == id);
+            if (_pro != null)
+            {
+                GetCart().Add_Product_Cart(_pro);
+            }
+            return RedirectToAction("ShowCart", "ShoppingCart");
+        }
+
+
+
+        public ActionResult Update_Cart_Quantity(FormCollection form)
+        {
+            Cart cart = Session["Cart"] as Cart;
+            string id_pro = form["idPro"];
+            int _quantity = int.Parse(form["cartQuantity"]);
+            cart.Update_quantity(id_pro, _quantity);
+            return RedirectToAction("ShowCart", "ShoppingCart");
+        }
+        public ActionResult RemoveCart(string id)
+        {
+            Cart cart = Session["Cart"] as Cart;
+            cart.Remove_CartItem(id);
+            return RedirectToAction("ShowCart", "ShoppingCart");
+        }
+        public PartialViewResult BagCart()
+        {
+            int total_quantity_item = 0;
+            Cart cart = Session["Cart"] as Cart;
+            if (cart != null)
+            {
+                total_quantity_item = cart.Total_quantity();
+            }
+            ViewBag.QuantityCart = total_quantity_item;
+            return PartialView("BagCart");
+        }
+        public ActionResult CheckOut(FormCollection form)
+        {
+            try
+            {
+                Cart cart = Session["Cart"] as Cart;
+                tbBill _order = new tbBill();
+                _order.billDate = DateTime.Now;
+                _order.idUser = int.Parse(form["Customer"]);
+                db.tbBills.Add(_order);
+                foreach (var item in cart.Items)
+                {
+                    tbDetailBill _order_detail = new tbDetailBill();
+                    _order_detail.idBill = _order.idBill;
+                    _order_detail.codePro = item._product.codePro;
+                    _order_detail.priceProBuying = (double)item._product.pricePro;
+                    _order_detail.quantityPro = item._quantity;
+                    db.tbDetailBills.Add(_order_detail);
+                }
+                db.SaveChanges();
+                cart.clearCart();
+                return RedirectToAction("CheckOut_Success", "ShoppingCart");
+            }
+            catch
+            {
+                return Content("Error checkout. Please check information of Customer...Thanks");
+            }
+        }
+        public ActionResult CheckOut_Success()
         {
             return View();
         }
---
+++ TheBakeryShop/Models/Cart.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheBakeryShop.Models
+{
+    public class CartItem
+    {
+        public tbProduct _product { get; set; }
+        public int _quantity { get; set; }
+    }
+    public class Cart
+    {
+        List<CartItem> items = new List<CartItem>();
+        public IEnumerable<CartItem> Items
+        {
+            get { return items; }
+        }
+        public void Add_Product_Cart(tbProduct _pro, int _quan = 1)
+        {
+            var item = Items.FirstOrDefault(s => s._product.codePro == _pro.codePro);
+            if (item == null)
+                items.Add(new CartItem
+                {
+                    _product = _pro,
+                    _quantity = _quan
+                });
+            else
+                item._quantity += _quan;
+        }
+        public int Total_quantity()
+        {
+            return items.Sum(s => s._quantity);
+        }
+        public decimal Total_money()
+        {
+            var total = items.Sum(s => s._quantity * s._product.pricePro);
+            return (decimal)total;
+        }
+        public void Update_quantity(string id, int _new_quan)
+        {
+            var item = items.Find(s => s._product.codePro == id);
+            if (item != null)
+                item._quantity = _new_quan;
+        }
+        public void Remove_CartItem(string id)
+        {
+            items.RemoveAll(s => s._product.codePro == id);
+        }
+        public void clearCart()
+        {
+            items.Clear();
+        }
+    }
+}
